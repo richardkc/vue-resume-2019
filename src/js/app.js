8afed910566799ccc9ae9a31775d55de22d27cb6
Richard <15917198755@163.com>
2019-09-17 12:45:25 +0800

    登录注册

--- src/js/app.js
+++ src/js/app.js
@@ -10,19 +10,55 @@ let app = new Vue({
             jobTitle:'前端工程师',
             email:'XXXXXX@xxx.com',
             phoneNumber:'xxxxxxxxxxx'
+        },
+        login:{
+            email:'',
+            password:''
+        },
+        signUp:{
+            name:'',
+            email:'',
+            password:''
         }
     },
     methods:{
         onEdit(key,value){
             this.resume[key] = value
         },
+        onLogin(e) {
+            AV.User.loginWithEmail(this.login.email, this.login.password).then(function (user) {
+                console.log(user)
+            }, function (error) {
+                if(error.code === 211){
+                    alert('用户不存在')
+                }else if(error.code === 210){
+                    alert('用户名和密码不匹配')
+                }
+            });
+        },
+        onSignUp(e) {
+            const user = new AV.User();
+            user.setUsername(this.signUp.name);
+            user.setPassword(this.signUp.password);
+            user.setEmail(this.signUp.email);
+            user.signUp().then(function (user) {
+                AV.User.logOut()
+            }, function (error) {
+            });
+        },
         onClickSave() {
-            var currentUser = AV.User.current();
+            let currentUser = AV.User.current();
             if(!currentUser){
                 this.loginVisible = true
             }else{
                 this.saveResume()
             }
+        },
+        saveResume(){
+            let {id} = AV.User.current()
+            var user = AV.Object.createWithoutData('User',id);
+            user.set('resume', this.resume);
+            user.save();
         }
     }
 })
--- src/js/av.js
+++ src/js/av.js
@@ -1,4 +1,4 @@
-/*var APP_ID = 'Jf7ptloLrvyXEGQDvRkXV2N4-gzGzoHsz';
+var APP_ID = 'Jf7ptloLrvyXEGQDvRkXV2N4-gzGzoHsz';
 var APP_KEY = 'NV1sBMJvAK7NJtBES69UHsy2';
 
 AV.init({
@@ -10,4 +10,4 @@ var testObject = new TestObject();
 testObject.set('words', 'Hello world!');
 testObject.save().then(function (testObject) {
     console.log('保存成功。')
-})*/
+})
