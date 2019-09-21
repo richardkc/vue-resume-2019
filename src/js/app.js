0c3f7a389e75680f08277b4e78bfa74b615675da
Richard <15917198755@163.com>
2019-09-21 15:17:14 +0800

    打印和预览功能

--- src/js/app.js
+++ src/js/app.js
@@ -4,9 +4,32 @@ let app = new Vue({
         loginVisible: false,
         signUpVisible: false,
         shareVisible: false,
+        escKey: false,
+        previewUser:{
+            objectId: undefined
+        },
         currentUser:{
             objectId:undefined,
             email:''
+        },
+        previewResume:{
+            name:'姓名',
+            gender:'男',
+            birthday:'1997年08月',
+            jobTitle:'前端工程师',
+            email:'XXXXXX@xxx.com',
+            phoneNumber:'xxxxxxxxxxx',
+            skills:[
+                {name: '请填写技能名称',description:'请填写技能描述'},
+                {name: '请填写技能名称',description:'请填写技能描述'},
+                {name: '请填写技能名称',description:'请填写技能描述'},
+                {name: '请填写技能名称',description:'请填写技能描述'}
+            ],
+            projects:[
+                {name:'请填写项目名称',link:'请填写链接',keyword:'请填写关键词',description:'请填写详细描述'},
+                {name:'请填写项目名称',link:'请填写链接',keyword:'请填写关键词',description:'请填写详细描述'}
+
+            ]
         },
         resume:{
             name:'姓名',
@@ -36,7 +59,29 @@ let app = new Vue({
             email:'',
             password:''
         },
-        shareLink:''
+        shareLink:'',
+        mode: 'edit' //'preview'
+    },
+    created() {  //全局监听键盘事件
+        var _this = this;
+        document.onkeydown = function (e) {
+            let key = window.event.keyCode;
+            if (key === 81) {
+                _this.escControl();
+            }
+        }
+    },
+    computed:{
+        displayResume(){
+            return this.mode === 'preview' ? this.previewResume : this.resume
+        }
+    },
+    watch:{
+        'currentUser.objectId': function(newValue,oldValue){
+            if(newValue){
+                this.getResume(this.currentUser)
+            }
+        }
     },
     methods:{
         onEdit(key,value){
@@ -104,13 +149,13 @@ let app = new Vue({
                 alert('保存失败')
             })
         },
-        getResume() {
+        getResume(user) {
             var query = new AV.Query('User');
-            query.get(this.currentUser.objectId).then((user) => {
+            return query.get(user.objectId).then((user) => {
                 let resume = user.toJSON().resume
-                Object.assign(this.resume,resume)
+                return resume
             },(error) => {
-                //异常处理
+                console.log(error)
             })
         },
         onLogout(e){
@@ -129,16 +174,37 @@ let app = new Vue({
         },
         removeproject(index){
             this.resume.projects.splice(index,1)
+        },
+        escControl(e){
+            this.escKey = !this.escKey
+        },
+        print(){
+            window.print()
         }
     }
 })
 
+//获取当前用户
 let currentUser = AV.User.current()
-if(currentUser){
+if (currentUser) {
     app.currentUser = currentUser.toJSON()
     app.shareLink = location.origin + location.pathname + '?user_id=' + app.currentUser.objectId
-    app.getResume()
+    app.getResume(app.currentUser).then((resume) => {
+        app.resume = resume
+    })
+}        
+
+let search = location.search
+let regex = /user_id=([^&]+)/
+let matches = search.match(regex)
+if (matches) {
+    let userId = matches[1]
+    app.mode = 'preview'
+    app.getResume({objectId: userId}).then((resume) => {
+        app.previewResume = resume
+    })
 }
+
 
 /*
 var User = AV.Object.extend('User');
